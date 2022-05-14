faef8af3b2b2d8363689ffb435bc4a8212984af3
xwinkX <opalyuk.e88@ukr.net>
2022-05-14 20:08:36 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const titleEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredient;
+  return itemEl;
+
+});
+
+titleEl.append(...elements);
+
+
