e5b3647a17674098f9e16e5f088e2279ffe5ae1a
xwinkX <opalyuk.e88@ukr.net>
2022-05-15 18:55:51 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,35 +1,14 @@
+
 const inputEl = document.querySelector('#validation-input');
-inputEl.addEventListener('input', onInput);
-// inputEl.addEventListener('blur', onInputBlur);
 
-// function onInput(event) {
-      
-//    event.currentTarget.value.length;
-      
-// }
-// console.log(onInput)
-
-
-// console.log(inputEl.getAttribute("data-length"));
-// function onInputBlur() {
-  
-//     if (onInput === inputEl.getAttribute("data-length")) {
-//         inputEl.setAttribute('class', 'valid');
-//     } 
-   
-//        inputEl.setAttribute('class', 'invalid');
-    
- 
-// }
-
-function onInput(event) {
-      
-    if (event.currentTarget.value.length === inputEl.getAttribute("data-length")) {
-        inputEl.setAttribute('class', 'valid');
+const borderColor = (event) => {
+    console.log(Number(inputEl.dataset.length)) 
+      console.log(event.target.value.length)
+    if (event.currentTarget.value.length === Number(inputEl.getAttribute("data-length"))) {
+       inputEl.setAttribute('class', 'valid');
     }
-    else { 
-    inputEl.setAttribute('class', 'invalid');
-    
+    else {
+        inputEl.setAttribute('class', 'invalid');
+    }
 } 
-}
-//  inputEl.addEventListener('blur', onInputBlur);
+inputEl.addEventListener('blur', borderColor);
