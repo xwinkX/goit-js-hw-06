624bb514a78788dc002f4a28f473b2be6f626775
xwinkX <opalyuk.e88@ukr.net>
2022-05-16 08:54:12 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,12 +2,18 @@ const form = document.querySelector('.login-form');
 
 form.addEventListener('submit', onFormSabmit);
 function onFormSabmit(event) {
-    event.preventDefault();
-    const formData = new formData(event.currentTarget);
-
+event.preventDefault();
+    const validForm = [...event.target.elements]
+        .filter((element) => element.tagName === "INPUT")
+        .some((element) => element.value === "");
+    if (validForm) {
+        alert('Всі поля повинні бути заповнені');
+    }
+    const formData = new FormData(event.currentTarget);
+    // console.lof(formData);
     formData.forEach((value, name) => {
-        
+        console.log('name', name);
+        console.log('value', value);
     })
-
-    console.log('www')
+    form.reset();
 }
