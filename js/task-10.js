012f3d02e2bd1a57026ad091f671474b28f32159
xwinkX <opalyuk.e88@ukr.net>
2022-05-16 09:39:52 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// const divEl = document.querySelector('#controls');
+// const boxEl = document.querySelector('#boxes');
+// const buttonCreateEl = document.querySelector('button[data-create');
+// const buttondestroyEl = document.querySelector('button[data-destroy');
+// const inputEl = document.querySelector('input');
+// buttonCreateEl.addEventListener('click', creatEl);
+// buttondestroyEl.addEventListener('click', destroyEl);
+
